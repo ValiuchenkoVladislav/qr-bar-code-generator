fca5d5e342489fc79011b33a10959f1af68376f8
vvvladislav <vladislavvaliuchenkov@gmail.com>
2023-07-01 18:13:24 -0700

    added main functionality

--- src/layout/Main/Main.tsx
+++ src/layout/Main/Main.tsx
@@ -1,10 +1,52 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+import Barcode from "react-barcode";
+import { QRCodeSVG } from "qrcode.react";
+
 import "./Main.scss";
 
 
+type CodeOptions = Readonly<{
+  type: "qr" | "bar";
+  url: string;
+}>;
+
 export function Main() {
+  const { register, handleSubmit, formState: { errors } } = useForm<CodeOptions>();
+  const [codeOptions, setCodeOptions] = useState<CodeOptions>();
+
   return (
     <main>
-      main
+      <form onSubmit={handleSubmit(setCodeOptions)} className="generation_form">
+        <div className="code_type_option" aria-invalid={!!errors.type}>
+          <label>
+            <input type="radio" value="qr" {...register("type", { required: true })}/>
+            QR Code
+          </label>
+          <label>
+            <input type="radio" value="bar" {...register("type", { required: true })}/>
+            QR Code
+          </label>
+        </div>
+
+        <input
+          type="url"
+          placeholder="Website URL"
+          {...register("url", { required: true, maxLength: 2048 })}
+          aria-invalid={!!errors.url}
+          className="url_input"
+        />
+
+        <button type="submit" className="submit_button">Generate</button>
+      </form>
+
+      <section className="generated_code">
+        {codeOptions?.type && ((codeOptions.type === "bar")
+          ? <Barcode value={codeOptions.url} displayValue={false} width={1}/>
+          : <QRCodeSVG value={codeOptions.url} size={220}/>
+        )}
+      </section>
     </main>
   );
 }
